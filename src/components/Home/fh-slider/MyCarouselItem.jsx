46eeed452c300feaf48be26a379694b205f02067
giannibussoletti <bussolettigianni@gmail.com>
2026-05-09 17:36:01 +0200

    fetched logo movies

--- src/components/Home/fh-slider/MyCarouselItem.jsx
+++ src/components/Home/fh-slider/MyCarouselItem.jsx
@@ -1,7 +1,40 @@
-import { Col, Container, Row } from "react-bootstrap"
+import { Col, Container, Image, Row } from "react-bootstrap"
 import MyButtonSlider from "./MyButtonCarousel"
+import { useEffect, useState } from "react"
 
 const MyCarouselItem = function (props) {
+  console.log(props)
+  const [logo, setLogo] = useState("")
+  const Auth =
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4N2ZlOGNmMGRmZmQ1NGI0ZmFmMTRlYzkzZjliOTViZCIsIm5iZiI6MTc3MTI4MjEzNC41NzIsInN1YiI6IjY5OTM5ZWQ2OTcxN2QwZGM5ZDA2NWE0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bbMQkik7cmt6uK6yP5WsuRlItQgQkkkeoH7ycPiJKAg"
+
+  const options = {
+    headers: {
+      Authorization: Auth,
+    },
+  }
+
+  const movieLogos = `https://api.themoviedb.org/3/movie/${props.infoMedia.id}/images?include_image_language=en-US`
+
+  const LogosFetching = () => {
+    fetch(movieLogos, options)
+      .then((response) => {
+        if (response.ok) {
+          return response.json()
+        } else {
+          throw new Error(response.status)
+        }
+      })
+      .then((data) => {
+        setLogo(data.logos[0])
+      })
+      .catch((err) => err)
+  }
+
+  useEffect(() => {
+    LogosFetching()
+  }, [])
+
   const buttonClass = "fw-bold px-5 py-2 text-capitalize shadow-sm me-2 mb-2 rounded-2 border-00"
 
   return (
@@ -13,8 +46,12 @@ const MyCarouselItem = function (props) {
           "url(http://image.tmdb.org/t/p/" + "original" + props.infoMedia.backdrop_path + ")",
       }}>
       <Row className="position-absolute mt-5" style={{ bottom: "25%" }}>
-        <Col className="mb-5 text-center text-md-start" xs={12} lg={6}>
-          <img className="w-75" src="#" alt="django logo" />
+        <Col className="mb-5 text-center text-md-end" xs={12} lg={6}>
+          <Image
+            className="w-75"
+            src={"http://image.tmdb.org/t/p/" + "w342" + logo.file_path}
+            alt="django logo"
+          />
         </Col>
         <Col xs={12} className="text-center text-md-start ms-md-5 ps-md-5">
           <MyButtonSlider
